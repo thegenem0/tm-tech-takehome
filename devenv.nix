abe646205a318142cd80f9b511a8cee0014a3492
thegenem0 <nemethgergo02@gmail.com>
2025-07-14 13:36:59 +0100

    build: adds local scripts for testing and coverage reports

--- devenv.nix
+++ devenv.nix
@@ -2,4 +2,14 @@
 
 {
   packages = with pkgs; [ go ];
+
+  scripts = {
+    go-test.exec = ''
+      go test -v ./...
+    '';
+
+    go-coverage.exec = ''
+      go test -coverprofile=coverage.out ./... && go tool cover -html=coverage.out
+    '';
+  };
 }
