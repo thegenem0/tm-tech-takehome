efb2c3d829b1b626893993ffdb21a6bcfd64ad5c
thegenem0 <nemethgergo02@gmail.com>
2025-07-14 13:54:45 +0100

    wip: adds edge case unit tests for GetTotalPrice

--- internal/checkout/checkout_test.go
+++ internal/checkout/checkout_test.go
@@ -1,6 +1,7 @@
 package checkout
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -37,6 +38,7 @@ func TestGetTotalPrice_EmptyBasket(t *testing.T) {
 	assert.Equal(t, 0, total)
 }
 
+// Tests simple basket with no offers
 func TestGetTotalPrice_SingleBasicItem(t *testing.T) {
 
 	// setup
@@ -55,6 +57,7 @@ func TestGetTotalPrice_SingleBasicItem(t *testing.T) {
 	assert.Equal(t, 20, total)
 }
 
+// Tests basket with single offer
 func TestGetTotalPrice_WithSpecialOffer(t *testing.T) {
 	// setup
 	pricingRules := setupPricingRulesWithOffers()
@@ -75,6 +78,90 @@ func TestGetTotalPrice_WithSpecialOffer(t *testing.T) {
 	// assert
 	assert.NoError(t, err)
 	assert.Equal(t, 130, total)
+}
+
+// Tests complex basket with multiple items and offers
+func TestGetTotalPrice_MixedBasketComplex(t *testing.T) {
+	// setup
+	checkout := NewCheckout(setupPricingRulesWithOffers())
+
+	// act
+
+	items := []string{"A", "B", "A", "B", "A", "A", "B", "A"}
+	for _, item := range items {
+		err := checkout.Scan(item)
+		assert.NoError(t, err)
+	}
+
+	total, err := checkout.GetTotalPrice()
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, 305, total)
+}
+
+func TestGetTotalPrice_IntOverflow(t *testing.T) {
+	// setup
+	pricingRules := pricing.Rules{
+		"A": {UnitPrice: math.MaxInt / 2},
+	}
+
+	checkout := NewCheckout(pricingRules)
+
+	// act
+
+	err := checkout.Scan("A")
+	assert.NoError(t, err)
+
+	err = checkout.Scan("A")
+	assert.NoError(t, err)
+
+	err = checkout.Scan("A") // Third scan should trigger int overflow
+	assert.NoError(t, err)
+
+	// assert
+
+	total, err := checkout.GetTotalPrice()
+	assert.NoError(t, err)
+
+	assert.Equal(t, math.MaxInt, total)
+}
+
+func TestGetTotalPrice_OfferWithZeroQuantity(t *testing.T) {
+	// setup
+	pricingRules := pricing.Rules{
+		"A": {UnitPrice: 50, Offer: &pricing.SpecialOffer{Quantity: 0, Price: 100}},
+	}
+	checkout := NewCheckout(pricingRules)
+
+	// act
+	err := checkout.Scan("A")
+	assert.NoError(t, err)
+
+	// assert
+	assert.NotPanics(t, func() {
+		total, err := checkout.GetTotalPrice()
+		assert.NoError(t, err)
+
+		assert.Equal(t, 50, total)
+	})
+}
+
+// Test for items that meet the exact eligibility boundary for offers
+func TestGetTotalPrice_MultipleExactOffers(t *testing.T) {
+	// setup
+	checkout := NewCheckout(setupPricingRulesWithOffers())
+
+	// act
+	for range 6 {
+		err := checkout.Scan("A")
+		assert.NoError(t, err)
+	}
+
+	// assert
+	total, err := checkout.GetTotalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 260, total)
 }
 
 // Tests invalid SKU appropriately return error
