c5ca3e0357af2685bffa6ca94cf7dac8bd695040
thegenem0 <nemethgergo02@gmail.com>
2025-07-14 12:51:52 +0100

    wip: adds unit test for scanning basic item

--- internal/checkout/checkout_test.go
+++ internal/checkout/checkout_test.go
@@ -10,10 +10,28 @@ func TestGetTotalPrice_EmptyBasket(t *testing.T) {
 	// setup
 	checkout := NewCheckout()
 
-	// call
+	// act
 	total, err := checkout.GetTotalPrice()
 
 	// assert
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, 0, total)
+}
+
+func TestGetTotalPrice_SingleBasicItem(t *testing.T) {
+
+	// setup
+	pricingRules := setupPricingRules()
+
+	// act
+	checkout := NewCheckout(pricingRules)
+
+	err := checkout.Scan("C")
+	assert.NoError(t, err)
+
+	// assert
+
+	total, err := checkout.GetTotalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 20, total)
 }
